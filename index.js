91bffb8e3cbda2fe390f7aa48158f4d418033ae0
AlaTomKing <alatomking@noreply.codeberg.org>
2025-02-26 18:14:01 +0100

    added an onion icon because onion skinning technique in animation

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const canvasEl = document.querySelector("#drawing-canvas")
-const ctx = canvasEl.getConext("2d")
+const ctx = canvasEl.getContext("2d")
 
+let resize = (e) => {
+    canvasEl.width = window.innerWidth
+    canvasEl.height = window.innerHeight
+
+    ctx.fillStyle = "#fff"
+    ctx.fillRect(20,40,100,100)
+}
+
+//window.onresize(resize)
+
+window.addEventListener("resize", resize)
+
+resize()
