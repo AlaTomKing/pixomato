f537b68c24b8eaa9a7e8462734016e79d7babdf3
Amogus Bread <89712796+AlaTomKing@users.noreply.github.com>
2025-02-27 13:33:20 +0100

    added github button

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+document.querySelector("#menu-github-btn").addEventListener("click", () => {
+  window.open("https://github.com/AlaTomKing/to-do-list")
+})
+
 // const canvasEl = document.querySelector("#drawing-canvas")
 // const ctx = canvasEl.getContext("2d")
 
@@ -17,10 +21,10 @@
 
 //resize()
 
-// window.addEventListener("load", () => {
-//     "use strict";
+window.addEventListener("load", () => {
+    "use strict";
     
-//     if ("serviceWorker" in navigator && document.URL.split(":")[0] !== "file") {
-//       navigator.serviceWorker.register("./sw.js");
-//     }
-// })
+    if ("serviceWorker" in navigator && document.URL.split(":")[0] !== "file") {
+      navigator.serviceWorker.register("./sw.js");
+    }
+})
