73fe86a1dd4a14f89f882b12e8ca5adc178651ec
AlaTomKing <alatomking@noreply.codeberg.org>
2025-02-27 12:49:28 +0100

    added offline support back but i havent tested yet. its supposed to update website

--- sw.js
+++ sw.js
@@ -8,18 +8,30 @@ let filesToCache = [
   "/style.css"
 ]
 
-self.addEventListener("install", function(e) {
+self.addEventListener('install',function(e){
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache)
-    })
-  )
-})
+      Promise.all([caches.open(STATIC_CACHE_NAME),caches.open(APP_CACHE_NAME),self.skipWaiting()]).then(function(storage){
+          var static_cache = storage[0];
+          var app_cache = storage[1];
+          return Promise.all([static_cache.addAll(CACHE_STATIC),app_cache.addAll(CACHE_APP)]);
+      })
+  );
+});
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request)
-    })
-  )
-})
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+      Promise.all([
+          self.clients.claim(),
+          caches.keys().then(function(cacheNames) {
+              return Promise.all(
+                  cacheNames.map(function(cacheName) {
+                      if (cacheName !== APP_CACHE_NAME && cacheName !== STATIC_CACHE_NAME) {
+                          console.log('deleting',cacheName);
+                          return caches.delete(cacheName);
+                      }
+                  })
+              );
+          })
+      ])
+  );
+});
