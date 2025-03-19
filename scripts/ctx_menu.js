9077436720784aae0f91f0ef88aad60aa2a87e1f
AlaTomKing <alatomking@noreply.codeberg.org>
2025-03-19 10:13:13 +0100

    the 100th commit

--- scripts/ctx_menu.js
+++ scripts/ctx_menu.js
@@ -5,45 +5,54 @@ let contextMenus
 const ctxFrameEl = document.getElementById("context-menu-frame")
 const ctxMenuEl = document.getElementById("context-menu")
 
+const ctxOffsetX = 1
+const ctxOffsetY = 1
+
 const addContextMenu = () => {
 
 }
 
 document.addEventListener("contextmenu", (e) => {
     e.preventDefault();
-
-    console.log(e.clientX, e.clientY);
-
-    console.log("context menu");
-
-    ctxFrameEl.style.display = "block";
-    ctxMenuEl.style.display = "block";
-
-    const rect = ctxMenuEl.getBoundingClientRect();
-
-    let posX = e.clientX;
-    let posY = e.clientY;
-
-    if (e.clientX + rect.width > window.innerWidth) {
-        posX = e.clientX - rect.width;
-    }
-
-    if (e.clientY + rect.height > window.innerHeight) {
-        posY = e.clientY - rect.height;
-    }
-
-    ctxMenuEl.style.left = posX + "px";
-    ctxMenuEl.style.top = posY + "px";
 });
 
 document.addEventListener("mousedown", (e) => {
+
     const rect = ctxMenuEl.getBoundingClientRect();
 
     if (!((e.clientX >= rect.left && e.clientX <= rect.left + rect.width) && (e.clientY >= rect.top && e.clientY <= rect.top + rect.height))) {
         ctxFrameEl.style.display = "none";
         ctxMenuEl.style.display = "none";
     }
+
 });
+
+document.addEventListener("mouseup", (e) => {
+    if (e.button === 2) {
+        console.log(e.clientX, e.clientY);
+
+        console.log("context menu");
+
+        ctxFrameEl.style.display = "block";
+        ctxMenuEl.style.display = "block";
+
+        const rect = ctxMenuEl.getBoundingClientRect();
+
+        let posX = e.clientX + ctxOffsetX;
+        let posY = e.clientY + ctxOffsetY;
+
+        if (posX + rect.width > window.innerWidth) {
+            posX = e.clientX - rect.width - ctxOffsetX;
+        }
+
+        if (posY + rect.height > window.innerHeight) {
+            posY = e.clientY - rect.height - ctxOffsetY;
+        }
+
+        ctxMenuEl.style.left = posX + "px";
+        ctxMenuEl.style.top = posY + "px";
+    }
+})
 
 window.addEventListener("blur", (e) => {
     console.log("windows blur")
