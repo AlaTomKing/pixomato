48206b0dba71d960aaa529d4c1b4ca1f78d1fd09
Amogus Bread <89712796+AlaTomKing@users.noreply.github.com>
2025-04-04 07:41:30 +0200

    more

--- scripts/file.js
+++ scripts/file.js
@@ -10,10 +10,15 @@ const encode_qoi = (data, desc) => {
      * - height
      */
 
+    console.log(desc)
+
     const maxSize = desc.width * desc.height * (desc.channels + 1) + 14 + 8;
 
-    let bytes = new ArrayBuffer(maxSize); // an array that holds bytes nad stuff
+    const buffer = new ArrayBuffer(maxSize);
+    let bytes = new Uint8Array(buffer); // an array that holds bytes nad stuff
     let i, p = 0;
+    let r, g, b, a;
+    let prev_r, prev_g, prev_b, prev_a;
 
     // #region 14-BYTE HEADER
 
@@ -24,27 +29,48 @@ const encode_qoi = (data, desc) => {
     bytes[p++] = 0x66; // f
 
     // width
-    bytes[p++] = desc.width;
-    bytes[p++] = desc.width;
-    bytes[p++] = desc.width;
-    bytes[p++] = desc.width;
+    bytes[p++] = (0xff000000 & desc.width) >> 24;
+    bytes[p++] = (0x00ff0000 & desc.width) >> 16;
+    bytes[p++] = (0x0000ff00 & desc.width) >> 8;
+    bytes[p++] = (0x000000ff & desc.width);
 
     // height
-    bytes[p++] = desc.height;
-    bytes[p++] = desc.height;
-    bytes[p++] = desc.height;
-    bytes[p++] = desc.height;
+    bytes[p++] = (0xff000000 & desc.height) >> 24;
+    bytes[p++] = (0x00ff0000 & desc.height) >> 16;
+    bytes[p++] = (0x0000ff00 & desc.height) >> 8;
+    bytes[p++] = (0x000000ff & desc.height);
 
-    // 
+    // channels & colorspace
+    bytes[p++] = desc.channels;
+    bytes[p++] = desc.colorspace;
 
     // #endregion
+
+    r = 0;
+    g = 0;
+    b = 0;
+    a = 255;
+    prev_r = r;
+    prev_g = g;
+    prev_b = b;
+    prev_a = a;
 
     // END PADDING
     for (i = 0; i < QOI_PADDING.length; i++) {
         bytes[p++] = QOI_PADDING[i];
     }
+
+    console.log(bytes, buffer);
 }
 
 const decode_qoi = (file) => {
 
+}
+
+const encode_png = (data) => {
+
+}
+
+const decode_png = (data) => {
+    
 }
