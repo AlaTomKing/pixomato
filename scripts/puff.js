c424aa5fedd554befbc3f7ad73605f6b9e11db89
AlaTomKing <alatomking@noreply.codeberg.org>
2025-04-14 01:53:05 +0200

    progress figuring out what to do with setjmp and next variable that uses an array of numbers instead of single

--- scripts/puff.js
+++ scripts/puff.js
@@ -20,27 +20,33 @@
 // This is some sort of a translation and a little bit of interpretation
 // of "puff.c" written in JavaScript by me, Tom!
 
-// This script does not have as many comments as the original one
-// but I might add them if I have the courage
+// I will comment anything different from "puff.c", while original comments
+// will still be put inside the code, e.g.: parameter types
 
-const MAXBITS = 15;
-const MAXLCODES = 286;
-const MAXDCODES = 30;
-const MAXCODES = (MAXLCODES + MAXDCODES);
-const FIXLCODES = 288;
+const SLOW = false;
+
+/*
+ * Maximums for allocations and loops.  It is not useful to change these --
+ * they are fixed by the deflate format.
+ */
+const MAXBITS = 15;               /* maximum bits in a code */           
+const MAXLCODES = 286;            /* maximum number of literal/length codes */
+const MAXDCODES = 30;             /* maximum number of distance codes */
+const MAXCODES = (MAXLCODES + MAXDCODES);  /* maximum codes lengths to read */
+const FIXLCODES = 288;            /* number of fixed literal/length codes */
 
 // JavaScript does not have struct so lets make a comment imagining on what
-// the struct should look like
+// the structs should look like
 
-/** state
+/** state (struct)
  * 
  * // output state
- * Uint8Array out       (unsigned char, 8-bit)
+ * Uint8Array out       (unsigned *char, 8-bit)
  * Number outlen        (unsigned long, 32-bit)
  * Number outcnt        (unsigned long, 32-bit)
  * 
  * // input state
- * Uint8Array in        (unsigned char, 8-bit)
+ * Uint8Array in        (unsigned *char, 8-bit)
  * Number inlen         (unsigned long, 32-bit)
  * Number incnt         (unsigned long, 32-bit)
  * Number bitbuf        (int,           16-bit)
@@ -51,3 +57,130 @@ const FIXLCODES = 288;
  * 
  */
 
+/*
+ * Return need bits from the input stream.  This always leaves less than
+ * eight bits in the buffer.  bits() works properly for need == 0.
+ *
+ * Format notes:
+ *
+ * - Bits are stored in bytes from the least significant bit to the most
+ *   significant bit.  Therefore bits are dropped from the bottom of the bit
+ *   buffer, using shift right, and new bytes are appended to the top of the
+ *   bit buffer, using shift left.
+ */
+const bits = (s, need) => { // s: state (struct), need: Number (int)
+    let val;
+
+    val = s.bitbuf;
+    while (s.bitcnt < need) {
+        if (s.incnt == s.inlen)
+            longjmp(s.env, 1);
+        val |= (s.in[s.incnt++]) << s.bitcnt;
+        s.bitcnt += 8;
+    }
+
+    s.bitbuf = (val >> need);
+    s.bitcnt -= need;
+
+    return (val & ((1 << need) - 1));
+}
+
+const stored = (s) => {
+    let len;
+
+    s.bitbuf = 0;
+    s.bitcnt = 0;
+
+    if (s.incnt + 4 > s.inlen)
+        return 2;
+    len = s.in[s.incnt++];
+    len |= s.in[s.incnt++] << 8;
+    if (s.in[s.incnt++] != (~len & 0xff) ||
+        s.in[s.incnt++] != ((~len >> 8) & 0xff))
+        return 2;
+    
+    if (s.incnt + len > s.inlen)
+        return 2;
+    if (s.out != null) {
+        if (s.outcnt + len > s.outlen)
+            return 1;
+        while (len--)
+            s.out[s.outcnt++] = s.in(s.incnt++);
+    } else {
+        s.outcnt += len;
+        s.incnt += len;
+    }
+
+    return 0;
+}
+
+/** huffman (struct)
+ * 
+ * Uint16Array count    (*short, 16-bit)
+ * Uint16Array symbol   (*short, 16-bit)
+ *
+ */
+
+const decode = (s, h) => {
+    if (SLOW) {
+        let len;
+        let code;
+        let first;
+        let count;
+        let index;
+
+        code = first = index = 0;
+        for (len = 1; len <= MAXBITS; len++) {
+            code |= bits(s, 1);
+            count = h.count[len];
+            if (code - count < first)
+                return h.symbol[index + (code - first)];
+            index += count;
+            first += count;
+            first <<= 1;
+            code <<= 1;
+        }
+        return -10;
+    } else {
+        let len;
+        let code;
+        let first;
+        let count;
+        let index;
+        let bitbuf;
+        let left;
+        let next;
+
+        bitbuf = s.bitbuf;
+        left = s.bitcnt;
+        code = first = index = 0;
+        len = 1;
+        next = h.count + 1;
+        while (true) {
+            while (left--) {
+                code |= bitbuf & 1;
+                bitbuf >>= 1;
+                count = next++;
+                if (code - count < first) { /* if length len, return symbol */
+                    s.bitbuf = bitbuf;
+                    s.bitcnt = (s.bitcnt - len) & 7;
+                    return h.symbol[index + (code - first)];
+                }
+                index += count;             /* else update for next length */
+                first += count;
+                first <<= 1;
+                code <<= 1;
+                len++;
+            }
+            left = (MAXBITS+1) - len;
+            if (left == 0)
+                break;
+            if (s.incnt == s.inlen)
+                longjmp(s.env, 1);         /* out of input */
+            bitbuf = s.in[s.incnt++];
+            if (left > 8)
+                left = 8;
+        }
+        return -10;                         /* ran out of codes */
+    }
+}
