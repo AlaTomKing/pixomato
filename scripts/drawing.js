d4fe8ccad842d5fdbd3c2b100182503bf604bb92
Amogus Bread <89712796+AlaTomKing@users.noreply.github.com>
2025-05-07 10:21:24 +0200

    oops

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -1,5 +1,54 @@
 // this script is responsible for drawing
 
+// note: channels value will always be 4
 (() => {
-    
+    let pos_X, pos_Y;
+    let mouse_down = false;
+    let in_canvas = false;
+
+    let canvas_width, canvas_height;
+    let current_pixel_data; // because layers exist
+    // let current_outside_pixel_data;
+
+    // #region the drawing part
+
+    // starts drawing and tracks the starting position of the mouse to calculate fair amount of distance
+    // between the mouse and it's pixelated neighbours to ensure no excess amounts of pixels are drawn ykwim
+    const startDrawing = (posX, posY) => {
+        mouse_down = true;
+    }
+
+    // stops tracking mouse
+    const stopDrawing = () => {
+        posX = null; pos_Y = null; mouse_down = false;
+    }
+
+    // sets color of pixel
+    const setColor = (r, g, b, a) => {
+
+    }
+
+    // adds one pixel
+    const setPixel = (x, y) => {
+        let r = 0, g = 0, b = 0, a = 255;
+
+        let position = y * canvas_width + x * 4;
+        current_pixel_data[position] = r;
+        current_pixel_data[position + 1] = g;
+        current_pixel_data[position + 2] = b;
+        current_pixel_data[position + 3] = a;
+    }
+
+    // always fires everytime a mouse moves. this is used during drawing and detection of mouse inside canvas
+    const mouseMoveEvent = (e) => {
+        if (mouse_down) {
+
+        }
+    }
+
+    // #endregion
+
+    // #region setup
+
+    // #endregion
 })();
--- scripts/project_class.js
+++ scripts/project_class.js
@@ -6,24 +6,29 @@ file extension: .pxmt
 
 */
 
-class layer {
-    constructor(name, fps) {
-        this.name = name;
-
-        this.settings = {
-            frames_per_second: 24,
-        }
-    }
+function frame() {
+    this.list = [
+        
+    ]
 }
 
-class pixomato_project {
-    constructor(name) {
-        this.name = name;
-        this.time_created = new Date().getTime();
-        this.layers = [];
+function layer(name, fps) {
+    this.name = name;
 
-        this.project_settings = {
-            frames_per_second: 24, // default fps that applies to all layers
-        }
+    this.settings = {
+        frames_per_second: 24,
+    }
+
+    this.frames
+}
+
+// represents project
+function pixomato_project(name, fps) {
+    this.name = name;
+    this.time_created = new Date().getTime();
+    this.layers = [];
+
+    this.project_settings = {
+        frames_per_second: 24, // default fps that applies to all layers
     }
 }
