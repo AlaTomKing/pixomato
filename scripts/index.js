3af22595790b22b08943c73cc4aacd93fd660a35
AlaTomKing <alatomking@noreply.codeberg.org>
2026-05-04 14:04:09 +0200

    in process of implementing tomatotools ui thingy

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 // the main script
 
-import { Window } from "tomatotools_ui/index"
+const finish_loading = () => {
+    const loading_screen = document.getElementById("loading");
+    const pixomato_app = document.getElementById("pixomato");
+    console.log("pixomato has loaded");
 
-    const finish_loading = () => {
-        const loading_screen = document.getElementById("loading");
-        const pixomato_app = document.getElementById("pixomato");
-        console.log("pixomato has loaded");
+    pixomato_app.style.display = "block";
+    // loading_screen.style.opacity = 0;
+    loading_screen.style.display = "none";
 
-        pixomato_app.style.display = "block";
-        // loading_screen.style.opacity = 0;
-        loading_screen.style.display = "none";
+    // setTimeout(() => {loading_screen.style.display = "none"}, 500)
+    start();
+}
 
-        // setTimeout(() => {loading_screen.style.display = "none"}, 500)
-        start();
-    }
+// the very first thing to do
+const start = () => {
+    setRootContainer(document.getElementById("widgets-frame"));
 
-    // the very first thing to do
-    const start = () => {
-        // create a flying window thingy
-        console.log("pixomato has started");
-    }
+    // create a flying window thingy
+    console.log("pixomato has started");
 
-    window.addEventListener("load", () => {
-        //setTimeout(finish_loading, 500);
-        finish_loading();
-    });
+    const test = new Window("Welcome to Pixomato!");
+
+    test.show();
+}
+
+window.addEventListener("load", () => {
+    //setTimeout(finish_loading, 500);
+    finish_loading();
+});
