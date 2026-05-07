550f72933e9b3bc231bda84acd18bb39857ae25f
AlaTomKing <alatomking@noreply.codeberg.org>
2026-05-08 00:16:22 +0200

    movable windows!!!

--- scripts/tomatotools_ui/index.js
+++ scripts/tomatotools_ui/index.js
@@ -2,6 +2,8 @@
 
 let rootContainer;
 
+const WINDOW_BORDER_GAP = 16;
+
 const setRootContainer = (container) => {
     rootContainer = container;
 }
@@ -14,8 +16,17 @@ class ComponentHandler {
 
 class Window extends ComponentHandler {
     static targetWindow;
+    static startPosX;
+    static startPosY;
+    static startMouseX;
+    static startMouseY;
 
     title = "New Window"
+
+    x;
+    y;
+    width = 300;
+    height = 200;
 
     constructor(title) {
         super();
@@ -23,8 +34,26 @@ class Window extends ComponentHandler {
         this.title = title;
     }
 
+    changePosition(x, y) {
+        this.x = Math.min(Math.max(x, -this.width + WINDOW_BORDER_GAP), rootContainer.offsetWidth - WINDOW_BORDER_GAP);
+        this.y = Math.min(Math.max(y, 0), rootContainer.offsetHeight - WINDOW_BORDER_GAP);
+
+        this.element.style.top = this.y + "px";
+        this.element.style.left = this.x + "px";
+    }
+
+    changeSize(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
     show() {
         console.log("show tomatotools");
+
+        if (!this.x || !this.y) {
+            this.x = Math.floor(rootContainer.offsetWidth / 2 - this.width / 2);
+            this.y = Math.floor(rootContainer.offsetHeight / 2 - this.height / 2);
+        }
 
         const element = document.createElement("div");
 
@@ -41,14 +70,20 @@ class Window extends ComponentHandler {
 
         element.className = "tt-window";
 
-        element.style.width = "300px";
-        element.style.height = "200px";
+        element.style.top = this.y + "px";
+        element.style.left = this.x + "px";
+        element.style.width = this.width + "px";
+        element.style.height = this.height + "px";
 
         const titleBar = element.querySelector(".tt-titleBar");
 
         titleBar.addEventListener("mousedown", (e) => {
             if (e.button === 0) {
                 Window.targetWindow = this;
+                Window.startPosX = this.x;
+                Window.startPosY = this.y;
+                Window.startMouseX = e.clientX;
+                Window.startMouseY = e.clientY;
             }
         })
 
@@ -59,13 +94,19 @@ class Window extends ComponentHandler {
         })
 
         rootContainer.appendChild(element);
+
+        this.element = element;
     }
 }
 
 // handle window dragging
 document.addEventListener("mousemove", (e) => {
     if (Window.targetWindow) {
-        console.log("move")
+        console.log("move", Window.startPosX, Window.startPosY);
+
+        Window.targetWindow.changePosition(Window.startPosX + e.clientX - Window.startMouseX,
+            Window.startPosY + e.clientY - Window.startMouseY
+        )
     }
 });
 
